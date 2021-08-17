5bab25c771a3c6eddca5a9f50e697634874e6960
ayy lmao <fck@off.potato>
2021-08-18 00:27:37 +0300

    disabled title menu blink

--- Graphics/ScreenTitleMenu scroll/default.lua
+++ Graphics/ScreenTitleMenu scroll/default.lua
@@ -4,7 +4,7 @@ return Def.ActorFrame {
 		Name="Main";
 		Text=gc:GetText();
 		InitCommand=function(self)
-			self:y(60):diffuseblink():effectperiod(0.5):effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0"))
+			self:y(60)--:diffuseblink():effectperiod(0.5):effectcolor1(color("1,1,1,1")):effectcolor2(color("1,1,1,0"))
 		end;
 		GainFocusCommand=function(self)
 			self:stoptweening():diffusealpha(1)
