96cae6de8ac780645ebf9a915bb8cf0047ea8ceb
krokodilas <fhjks@gmafdghilhjhgj.cgfhj>
2022-01-09 19:36:03 +0200

    Theme options and metrics.ini cleanup

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,8 +1,8 @@
 return Def.ActorFrame{
-	LoadActor("troopers_dance")..{
+	--[[LoadActor("troopers_dance")..{
 	InitCommand=function(self)
 		self:zoomx(SCREEN_WIDTH/640)
 	end;
 	Condition=GAMESTATE:GetCurrentGame():GetName() ~= "beat";
-	};
+	};]]
 }
